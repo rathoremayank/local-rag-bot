06d73575e6fbb5452a4d649b7201395f31c96b56
rathoremayank <devopsadm@md62y37c.ad001.siemens.net>
2025-07-18 14:49:48 +0530

    added ui

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.rag_engine import get_qa_chain
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 qa_chain = get_qa_chain()
+
+# Add this block
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify ["http://localhost:5500"] etc.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Query(BaseModel):
     question: str
